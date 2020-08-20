b83e8018477232eb4e1d7928ae54b828b17ff0eb
Denise Henderson <denise@henderson3.us>
2020-08-20 23:04:40 +0000

    update

--- lib/wine_info/api.rb
+++ lib/wine_info/api.rb
@@ -6,8 +6,8 @@ require 'json'
 class WineInfo::API 
   
   def self.get_info
-    #uri = URI.parse('https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=petrus&vintage=2011')
-    url = "https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=molly+dooker&vintage=2&currencycode=USD"
+    url = "https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=petrus&vintage=2011"
+   # url = "https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=molly+dooker&vintage=2&currencycode=USD"
     uri = URI.parse(url)
     #uri = URI.parse('https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=molly+dooker&vintage=2&currencycode=USD')
     response = Net::HTTP.get_response(uri)
