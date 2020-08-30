6b582deccebe2c6675168b83e5a506fe16eba047
Denise Henderson <denise@henderson3.us>
2020-08-30 15:42:43 +0000

    more troubleshooting error handling and menu manipulation

--- lib/wine_info/api.rb
+++ lib/wine_info/api.rb
@@ -18,14 +18,14 @@ class WineInfo::API
       # response = Net::HTTP.get_response(uri)
       # response_hash = JSON.parse(response.body)
       #binding.pry
-      hold_hash_array = {"return-code"=>1, "return-comment"=>"No matching wines", "list-count"=>0}
+      hold_hash_array << {"return-code"=>1, "return-comment"=>"No matching wines", "list-count"=>0}
       #hold_hash_array << response_hash
       vintage += 1
     end #do
     binding.pry
     # response_hash.each do
     if (hold_hash_array[0]["return-code"]) != nil &&  (hold_hash_array[0]["return-code"])== 8
-        WineInfo::CLI.menu(message="Too Many Matches Were Found. Please Narrow Your Search to a Specific Producer",wine_desc=nil)
+        WineInfo::CLI.new.menu(message="Too Many Matches Were Found. Please Narrow Your Search to a Specific Producer",wine_desc=nil)
       elsif hold_hash_array.length > 0 && (hold_hash_array[0]["return-code"]) != nil && (hold_hash_array[0]["return-code"])==0
         WineInfo::Wine.new(wine_desc,vintage=2015,hold_hash_array[0])
       elsif hold_hash_array.length > 1 && (hold_hash_array[1]["return-code"])!= nil && (hold_hash_array[1]["return-code"])==0
@@ -34,7 +34,7 @@ class WineInfo::API
         WineInfo::Wine.new(wine_desc,vintage=2017,hold_hash_array[2])
       elsif hold_hash_array.length >3 && (hold_hash_array[3]["return-code"])!= nil && (hold_hash_array[3]["return-code"])==0
         WineInfo::Wine.new(wine_desc,vintage=2018,hold_hash_array[3])
-      else WineInfo::CLI.menu(message="Please Enter a Valid Producer",wine_desc=nil) 
+      else WineInfo::CLI.new.menu(message="Please Enter a Valid Producer",wine_desc=nil) 
     end
       
       # WineInfo::CLI.new(message="No Wine Matches Were Found. Please Enter A Valid Producer and Wine")
