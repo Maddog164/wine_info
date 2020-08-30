238c819cf86ef751bc204707d66fefa1cc58dfb3
Denise Henderson <denise@henderson3.us>
2020-08-30 22:16:37 +0000

    More Menu manipulation and error handling

--- lib/wine_info/cli.rb
+++ lib/wine_info/cli.rb
@@ -3,33 +3,40 @@ class WineInfo::CLI
 
   attr_accessor :message, :wine_desc
   
-    def list_wineinfo(klass,wine_desc)
+    def wait_for_response(resp_message)
+      puts "#{resp_message}"
+      puts "PRESS ANY KEY TO RETURN TO MENU"
+      wine_details_enter = gets.chomp
+      system("clear")
+    end
+      
+  
+    def list_wineinfo(klass,message,wine_desc)
       i = 0 
       #binding.pry
+      puts "#{message}"
       hold_wine_desc = ""
-      klass.all.each do |wine_desc|
+      klass.all.each do |inside_desc|
       #binding.pry
-      # if wine_desc == nil || wine_desc == klass.all[i]["wine_desc"]
+      if (wine_desc != nil && wine_desc == klass.all[i]["wine_desc"]) || wine_desc == nil
+        #|| wine_desc == klass.all[i]["wine_desc"]
         if hold_wine_desc != klass.all[i]["wine_desc"]
           puts ("Wine Description #{klass.all[i]["wine_desc"].split.map(&:capitalize).join(" ")}")
           hold_wine_desc = klass.all[i]["wine_desc"]
         end #if
         puts ("   #{klass.all[i]["vintage"]}  Average Price  #{klass.all[i]["price_avg"]}   Score: #{klass.all[i]["ws_score"]}")
         #puts "#{klass.all[i]["wine_desc"]}"
-      #end #if
+      end #if
         i += 1
     end #do
-    puts "PRESS ANY KEY TO RETURN TO MENU"
-    wine_detals_enter = gets.chomp
-    system("clear")
+    wait_for_response(resp_message = "")
     
   end 
     #call
     
   def menu(message, wine_desc)
     if message == "Here are the results of your search:"
-      puts message
-      list_wineinfo(klass=WineInfo::Wine,wine_desc)
+      list_wineinfo(klass=WineInfo::Wine,message,wine_desc)
       #call
     else
       puts message
@@ -42,6 +49,7 @@ class WineInfo::CLI
   def call
     kp_proc = true
     while kp_proc==true do
+      system("clear")
       puts "Wine Info Menu"
       puts "What Would You Like To Do?"
       puts ("1. Perform Wine Search")
@@ -54,7 +62,11 @@ class WineInfo::CLI
         when "1"
         menu(@message = "Enter the Wine You Would Like to Look Up", wine_desc=nil) 
         when "2"
-        list_wineinfo(klass=WineInfo::Wine,wine_desc=nil)
+          if WineInfo::Wine.all.length > 0
+            list_wineinfo(klass=WineInfo::Wine,message="Your search results for this session",wine_desc=nil)
+          else
+            wait_for_response (message= "You don't have any Search data yet!!")
+          end #if
         when "3"
       
         when "9"
