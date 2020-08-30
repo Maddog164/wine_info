6743b1bf96290d65c4a90af69e0d225061bef4e4
Denise Henderson <denise@henderson3.us>
2020-08-30 21:43:35 +0000

    Modifications to menu manipulation

--- lib/wine_info/cli.rb
+++ lib/wine_info/cli.rb
@@ -3,40 +3,13 @@ class WineInfo::CLI
 
   attr_accessor :message, :wine_desc
   
-  # def initialize(message="Enter the Wine You Would Like to Look Up",wine_desc=nil)
-  #   @message = message
-  #   menu(message, wine_desc)
-  # end
-  
-  # def call 
-  #   puts "Welcome to Wine Info!!"
-  #   @message = "Enter the Wine You Would Like to Look Up"
-  #   binding.pry
-  #   self.menu(@message, wine_desc=nil)
-  # end
-  
-  # def menu(message, wine_desc)
-  #   if message == "Here are the results of your search:"
-  #     puts message
-  #     list_wineinfo(klass=WineInfo::Wine,wine_desc)
-  #   else
-  #     puts message
-  #     input = gets.chomp
-  #     #binding.pry
-  #     WineInfo::API.get_info(input)
-  #   end
-  # end
-  
     def list_wineinfo(klass,wine_desc)
-    # if wine_desc != nil
-    #   nice_wine_desc = wine_desc.split.map(&:capitalize).join(" ")
-    #   puts "#{nice_wine_desc}"
-    i = 0 
-    #binding.pry
-    hold_wine_desc = ""
-    klass.all.each do |wine_desc|
-    #binding.pry
-     # if wine_desc == nil || wine_desc == klass.all[i]["wine_desc"]
+      i = 0 
+      #binding.pry
+      hold_wine_desc = ""
+      klass.all.each do |wine_desc|
+      #binding.pry
+      # if wine_desc == nil || wine_desc == klass.all[i]["wine_desc"]
         if hold_wine_desc != klass.all[i]["wine_desc"]
           puts ("Wine Description #{klass.all[i]["wine_desc"].split.map(&:capitalize).join(" ")}")
           hold_wine_desc = klass.all[i]["wine_desc"]
@@ -44,63 +17,53 @@ class WineInfo::CLI
         puts ("   #{klass.all[i]["vintage"]}  Average Price  #{klass.all[i]["price_avg"]}   Score: #{klass.all[i]["ws_score"]}")
         #puts "#{klass.all[i]["wine_desc"]}"
       #end #if
-      i += 1
+        i += 1
     end #do
-    call
-      
-  #   while i < 4 && klass.all[i] != nil do
-  #     if ii = 0 
-  #       puts "#{klass.all[0]["wine_desc"]}.split.map(&:capitalize).join(" ")"
-  #       ii+=1
-  #     end #if
-  #     puts "   Vintage #{klass.all[i]["vintage"]}  Average Price: #{klass.all[i]["price_avg"]}  Wine Score: #{klass.all[i]["ws_score"]}"
-  #     i+=1
-  #   end
-  # end
-    #binding.pry
-    #puts klass.all
-    # hash_values = klass.all.values 
+    puts "PRESS ANY KEY TO RETURN TO MENU"
+    wine_detals_enter = gets.chomp
+    system("clear")
     
-    #klass.all
-    #   "#{vintage}, Average Price: #{price_avg}   Wine Score: #{ws_score}"
-    # end
-  end
-  
+  end 
+    #call
+    
   def menu(message, wine_desc)
     if message == "Here are the results of your search:"
       puts message
       list_wineinfo(klass=WineInfo::Wine,wine_desc)
-      call
-      
+      #call
     else
       puts message
       wine_input = gets.chomp
       #binding.pry
       WineInfo::API.get_info(wine_input)
-    end
+    end #if
   end
   
-  def call 
-    puts "Wine Info Menu"
-    puts "What Would You Like To Do?"
-    puts ("1. Perform Wine Search")
-    puts ("2. Show Me All Results For This Session")
-    puts ("3. Delete a Wine/Producer")
-    puts ("9. Quit")
-    user_input = gets.chomp
-    case user_input
-      when "1"
-       menu(@message = "Enter the Wine You Would Like to Look Up", wine_desc=nil) 
-      when "2"
-       list_wineinfo(klass=WineInfo::Wine,wine_desc=nil)
-      when "3"
+  def call
+    kp_proc = true
+    while kp_proc==true do
+      puts "Wine Info Menu"
+      puts "What Would You Like To Do?"
+      puts ("1. Perform Wine Search")
+      puts ("2. Show Me All Results For This Session")
+      puts ("3. Delete a Wine/Producer")
+      puts ("9. Quit")
+      user_input = gets.chomp
+      system("clear")
+      case user_input
+        when "1"
+        menu(@message = "Enter the Wine You Would Like to Look Up", wine_desc=nil) 
+        when "2"
+        list_wineinfo(klass=WineInfo::Wine,wine_desc=nil)
+        when "3"
       
-      when "9"
-        puts "Goodbye . . ."
-        'exit'
+        when "9"
+          puts "Goodbye . . ."
+          kp_proc = false
       else
-       call
-    end #case
+        puts "Please make a valid selection"
+      end #case
+    end #do
   end #call
     
      
