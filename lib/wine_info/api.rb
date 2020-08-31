db59fc0c628ebb2288ae6333d4bfa9e18f04ff30
Denise Henderson <denise@henderson3.us>
2020-08-31 01:20:52 +0000

    opening up api to workk with live data

--- lib/wine_info/api.rb
+++ lib/wine_info/api.rb
@@ -14,13 +14,13 @@ class WineInfo::API
     system("clear")
     puts "Retrieving your information for #{wine_desc}"
     hold_hash_array = []
-    vintage = 2017
+    vintage = 2015
     until vintage == 2019 do
       url = "https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=#{wine_desc}&vintage=#{vintage}&format=J"
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
       response_hash = JSON.parse(response.body)
-      binding.pry
+      #binding.pry
       hold_hash_array << response_hash
       if (hold_hash_array[0]["return-code"]) != nil &&  (hold_hash_array[0]["return-code"])== 8
           WineInfo::CLI.new.menu(message="Too Many Matches Were Found. Please Narrow Your Search to a Specific Producer",wine_desc=nil)
@@ -32,8 +32,8 @@ class WineInfo::API
     # response_hash.each do
     vintage = 2015
     i = 0
-    while i < 2 do
-      binding.pry
+    while i < 4 do
+      #binding.pry
       if hold_hash_array.length > i && (hold_hash_array[i]["return-code"])==0
           WineInfo::Wine.new(wine_desc,vintage,hold_hash_array[0])
           i += 1
