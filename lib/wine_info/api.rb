6d013e44e36bfa1b76a7f5fa7da25d4d73741385
Denise Henderson <denise@henderson3.us>
2020-09-01 09:59:15 +0000

    Finalized error handling for invalid return codes

--- lib/wine_info/api.rb
+++ lib/wine_info/api.rb
@@ -15,7 +15,8 @@ class WineInfo::API
     puts "Retrieving your information for #{wine_desc}"
     hold_hash_array = []
     vintage = 2015
-    until vintage == 2019 do
+    continue_var = true
+    until vintage == 2019 || continue_var == false do
       url = "https://api.wine-searcher.com/x?api_key=test6ws20200819ur&winename=#{wine_desc}&vintage=#{vintage}&format=J"
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
@@ -23,7 +24,8 @@ class WineInfo::API
       #binding.pry
       hold_hash_array << response_hash
       if (hold_hash_array[0]["return-code"]) != nil &&  (hold_hash_array[0]["return-code"])== 8
-          WineInfo::CLI.new.menu(message="Too Many Matches Were Found. Please Narrow Your Search to a Specific Producer",wine_desc=nil)
+        continue_var = false 
+        WineInfo::CLI.new.menu(message="Too Many Matches Were Found. Please Narrow Your Search to a Specific Producer",wine_desc=nil)
       end
       vintage += 1
       #binding.pry
@@ -32,19 +34,22 @@ class WineInfo::API
     # response_hash.each do
     vintage = 2015
     i = 0
-    while i < 4 do
+    while i < 4 && continue_var == true do
       #binding.pry
       if hold_hash_array.length > i && (hold_hash_array[i]["return-code"])==0
           WineInfo::Wine.new(wine_desc,vintage,hold_hash_array[i])
           i += 1
           vintage += 1
-        else hold_hash_array.length > i && (hold_hash_array[i]["return-code"])==1 
-          i += 1
-          vintage += 1
-        #else WineInfo::CLI.new.menu(message="Please Enter a Valid Producer",wine_desc=nil) 
-      end #if
+        elsif hold_hash_array.length == 4 && (hold_hash_array[i]["return-code"])==1 && (hold_hash_array[i+1]["return-code"]==1) && (hold_hash_array[i+2]["return-code"]==1) && (hold_hash_array[i+3]["return-code"]==1)
+          # i += 1
+          # vintage += 1
+          continue_var = false
+          WineInfo::CLI.new.menu(message="Please Enter a Valid Producer",wine_desc=nil)
+        end #if
     end #do
-    WineInfo::CLI.new.menu(message="Here are the results of your search:",wine_desc)
+    if continue_var == true
+      WineInfo::CLI.new.menu(message="Here are the results of your search:",wine_desc)
+    end #if
    
   end
   
